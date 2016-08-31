ef27d5b46024eb785e409e5213014bce53719ae4
Ylia Fatkullina <ylia.fatk@gmail.com>
2016-09-01 00:23:20 +0500

    Добавлены провайдеры погодныйх сервисов

---
+++ MyWeatherForecast/Services/WeatherProviders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeatherForecast.Services
+{
+    public class WeatherProviders : IWeatherProviders
+    {
+        private IDictionary<string, IWeatherProvider> _providers = new Dictionary<string, IWeatherProvider>
+        {
+            {"Openweathermap", new OpenweatherMapProvider()},
+            {"WunderGround", new WundergroundProvider()}
+        };
+
+
+        public IDictionary<string, IWeatherProvider> Providers
+        {
+            get { return _providers; }
+            set { _providers = value; }
+        }
+
+        public void Add(string providerName, IWeatherProvider provider)
+        {
+            _providers.Add(providerName, provider);
+        }
+    }
+}
