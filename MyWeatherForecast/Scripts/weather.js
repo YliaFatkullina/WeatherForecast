d32117558585c0314fb5fc053ae42018d6454033
Ylia Fatkullina <ylia.fatk@gmail.com>
2016-08-31 22:04:14 +0500

    Обновление пакетов

---
+++ MyWeatherForecast/Scripts/weather.js
@@ -0,0 +1,23 @@
+﻿jQuery.validator.addMethod("lettersonly", function (value, element) {
+    return this.optional(element) || /^[- A-Яа-я\s]+$/i.test(value);
+}, "Некорретные символы в названии");
+
+
+$().ready(function () {
+    $("#formCity").validate(({
+        onkeyup: false,
+        rules: {
+            cityName: {
+                required: true,
+                minlength: 3,
+                lettersonly: true
+            }
+        },
+        messages: {
+            cityName: {
+                required: "Введите название города",
+                minlength: "Название города должно содержать не меньше 3 символов"
+            }
+        }
+    }));
+});
