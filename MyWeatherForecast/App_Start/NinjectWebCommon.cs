b0c41f4cfb837ea089ca9db14f4c302f750d6c1c
Ylia Fatkullina <ylia.fatk@gmail.com>
2016-09-01 00:42:19 +0500

    Подготовка к внедрению зависимостей

--- MyWeatherForecast/App_Start/NinjectWebCommon.cs
+++ MyWeatherForecast/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace MyWeatherForecast.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
---
+++ MyWeatherForecast/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWeatherForecast.Services;
+using MyWeatherForecast.ViewModels;
+using Ninject;
+
+namespace MyWeatherForecast.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            _kernel.Bind<IWeatherProviders>().To<WeatherProviders>();
+            _kernel.Bind<IWeatherForecast>().To<Openweathermap>();
+            _kernel.Bind<IWeatherForecast>().To<Wunderground>();
+        }
+    }
+}
