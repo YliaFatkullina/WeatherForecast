1776988bbb28c63808adf8a56144a7850f45d0d5
Ylia Fatkullina <ylia.fatk@gmail.com>
2016-08-31 22:07:09 +0500

    Добавлен метод контроллера для отображения частичного представления

--- MyWeatherForecast/Controllers/HomeController.cs
+++ MyWeatherForecast/Controllers/HomeController.cs
@@ -1,46 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Web;
 using System.Web.Mvc;
-using MyWeatherForecast.Models;
 using MyWeatherForecast.Services;
+using MyWeatherForecast.ViewModels;
 
 namespace MyWeatherForecast.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string cityName)
+        public ActionResult Index()
         {
-            Weather test = null;
-            List<IWeatherModel> weathers = new List<IWeatherModel>();
+            return View();
+        }
 
-            if (cityName != null)
+        [HttpPost]
+        public ActionResult GetWeatherForecast(string cityName)
+        {
+            if(cityName != null)
             {
-                ViewData["city"] = cityName;
+                return PartialView("WeatherForecastPartial", SearchCity(cityName.Trim()));
+            }
+            return PartialView("WeatherForecastPartial");
+        }
 
-                string citId = "";
-                IWeatherService owm = new OpenweathermapService();
-                IWeatherModel weatherForecast = owm.GetForecast(cityName.Trim());
-                if (weatherForecast != null)
-                {
-                    weathers.Add(weatherForecast);
-                    citId = weatherForecast.Id;
-                }
-                    
-                IWeatherService wg = new WundergroundService();
-                IWeatherModel weatherData = wg.GetForecast(cityName.Trim());
-                if (weatherData != null)
-                    weathers.Add(weatherData);
+        private List<IWeatherModel> SearchCity(string cityName)
+        {
+            
+            ViewData["city"] = cityName;
 
+            List<IWeatherModel> weathers = new List<IWeatherModel>();
+            string citId = "";
+            IWeatherService owm = new OpenweathermapService();
+            IWeatherModel weatherForecast = owm.GetForecast(cityName);
+            if (weatherForecast != null)
+            {
+                weathers.Add(weatherForecast);
+                citId = weatherForecast.Id;
+            }
+
+            IWeatherService wg = new WundergroundService();
+            IWeatherModel weatherData = wg.GetForecast(cityName);
+            if (weatherData != null)
+                weathers.Add(weatherData);
+
+            if (weathers.Count != 0)
+            {
                 var citiesCookie = Request.Cookies["cities"] ?? new HttpCookie("cities");
-                
                 citiesCookie[citId] = cityName;
                 citiesCookie.Expires = DateTime.Now.AddDays(1);
-
                 Response.Cookies.Add(citiesCookie);
             }
-            return View(weathers);
+            
+            return weathers;
         }
 
     }
