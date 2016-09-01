8bf117b9221c01edb23a1fe3e7dffa8216cbc680
Ylia Fatkullina <ylia.fatk@gmail.com>
2016-09-01 22:00:10 +0500

    Добавлен пропущенный интерфейс

---
+++ MyWeatherForecast/Services/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MyWeatherForecast.Services
+{
+    public interface IWeatherService
+    {
+        IDictionary<string, IWeatherProvider> Providers { get; set; }
+    }
+}
